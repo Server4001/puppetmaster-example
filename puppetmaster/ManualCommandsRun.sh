8cbd47aae434c4e8c017093f8ebbbaaa37c5f23a
Brice Bentler <server4001@gmail.com>
2016-08-21 18:33:27 -0700

    Bumps ruby version to 2.0.0 and adds install of r10k

--- puppetmaster/ManualCommandsRun.sh
+++ puppetmaster/ManualCommandsRun.sh
@@ -7,28 +7,45 @@ sudo service ntpd start
 sudo chkconfig ntpd on
 
 
-# Install Ruby:
-sudo yum install -y ruby-devel ruby rubygems
+# Install Ruby, Puppet, and r10k:
+sudo su
+command curl -sSL https://rvm.io/mpapis.asc | sudo gpg2 --import -
+curl -sSL https://get.rvm.io | bash -s stable
+source /etc/profile.d/rvm.sh
+rvm requirements
+rvm install 2.0.0
+rvm use 2.0.0 --default
+mkdir /root/puppet-source
+cp /vagrant/puppet_source/*.tar.gz /root/puppet-source/
+cd ~/puppet-source/
+tar -zxvf hiera-3.2.1.tar.gz
+tar -zxvf puppet-3.8.7.tar.gz
+rm -rf *.tar.gz
+cd hiera-3.2.1/
+ruby install.rb
+cd ..
+cd puppet-3.8.7/
+gem install facter
+ruby install.rb
+puppet resource group puppet ensure=present
+puppet resource user puppet ensure=present gid=puppet shell='/sbin/nologin'
+cp /vagrant/puppet_source/puppetmaster.init.d /etc/init.d/puppetmaster
+chmod 755 /etc/init.d/puppetmaster
+mv /etc/init.d/puppetmaster /etc/init.d/puppet
+gem install r10k
+exit
 
 
 # Install Passenger and it's dependencies:
-sudo gem install rake -v 10.5.0
-sudo gem install rack -v 1.6.4
-sudo gem install passenger -v 5.0.29
+sudo su
+gem install rake -v 10.5.0
+gem install rack -v 1.6.4
+gem install passenger -v 5.0.29
+exit
 
 
 # Install Apache:
 sudo yum install -y httpd
-
-
-# Install Puppet:
-sudo rpm -ivh http://yum.puppetlabs.com/puppetlabs-release-el-6.noarch.rpm
-sudo yum install -y puppet
-
-
-# Install puppet master server:
-sudo yum install -y puppet-server
-puppet master --version # 3.8.7
 
 
 # Setting up the directory environments:
@@ -45,7 +62,9 @@ sudo sed -i 's\=enforcing\=permissive\g' /etc/sysconfig/selinux
 
 # Generate the SSL certificates used by puppet master:
 # Start puppet master. Each time master boots up, it checks if the certs exist. If not, then master creates them.
-sudo puppet master --verbose --no-daemonize
+sudo su
+puppet master --verbose --no-daemonize
+exit
 
 
 # Make sure iptables allows puppet to accept requests on port 8140. NOTE: I did not have to do this because iptables
@@ -56,13 +75,18 @@ sudo service iptables restart
 
 # Puppet master is a ruby on rails app, so we need to setup Apache and Phusion Passenger:
 sudo yum -y install httpd-devel mod_ssl gcc gcc-c++ libcurl-devel openssl-devel
-sudo passenger-install-apache2-module
+sudo su
+passenger-install-apache2-module
 # [Press enter]
 # [Ensure that ruby is the only thing selected and press enter]
 # When the install completes, it will give you a snippet to add to your apache configuration. Use this to update the config at: files/puppetmaster.conf
+exit
 
 # Creating configuration for puppet master:
 sudo mkdir -p /usr/share/puppet/rack/puppetmasterd/{public,tmp}
+
+# If config.ru is not present, copy files/config-rack.ru, which I got from:
+# https://github.com/puppetlabs/puppet/blob/3.8.7/ext/rack/config.ru
 sudo cp /usr/share/puppet/ext/rack/config.ru /usr/share/puppet/rack/puppetmasterd/
 sudo chown puppet:puppet /usr/share/puppet/rack/puppetmasterd/config.ru
 
@@ -77,8 +101,10 @@ sudo chkconfig httpd on
 
 
 # After setting up the agent on the other centos and ubuntu server, and sending the certificate sign requests, we signed the certificates:
-sudo puppet cert sign wiki
-sudo puppet cert sign wikitest
+sudo su
+puppet cert sign wiki
+puppet cert sign wikitest
+exit
 
 
 # Copy allonone/provision/production/manifests to /etc/puppet/environments/production/manifests
